61538e8f94a2668e88f7f0d5a6fc2d92b419b88e
unknown <anup.kore88@gmail.com>
2023-04-09 22:31:09 +0530

    First

---
+++ First.py
@@ -0,0 +1,20 @@
+print("Hello from python")
+
+var = 45
+print(var)
+
+var1 = 5
+var2 = 10
+print(var1+var2)
+
+var3 = input()
+print(var3)
+
+#var4 = input()
+#var5 = input()
+#print(var4+var5)
+
+var4 = input()
+var5 = input()
+var6 = int (var4) + int (var5) 
+print(var6)
